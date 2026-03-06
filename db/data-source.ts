49b96dcb51c7954ee50e020cb2c5ce74c39b7032
Yurii Vydai <yuriivydai@gmail.com>
2026-03-06 20:43:51 +0200

    migrations

---
+++ db/data-source.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config(); // Для локальної розробки
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.RDS_HOSTNAME || process.env.DB_HOSTNAME,
+  port: parseInt(process.env.RDS_PORT || process.env.DB_PORT || '3306', 10),
+  username: process.env.RDS_USERNAME || process.env.DB_USERNAME,
+  password: process.env.RDS_PASSWORD || process.env.DB_PASSWORD,
+  database: process.env.RDS_DB_NAME || process.env.DB_NAME,
+  entities: ['dist/**/entities/*.entity.js'], // Вказуємо на JS у dist!
+  migrations: ['dist/migrations/*.js'],
+  synchronize: false,
+});
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
+import { AppDataSource } from '../db/data-source';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'example',
-      database: 'db1',
-      entities: [User],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(AppDataSource.options),
     UsersModule,
   ],
   controllers: [AppController],
